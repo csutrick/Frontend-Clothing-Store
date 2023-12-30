9694e8d993146b17e507bca97cd9cd30aa970aa8
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-12-29 19:40:16 -0600

    Create react app, added tailwinds

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-full py-4 flex flex-row items-center justify-center">
+      <h1 className="text-4xl text-black font-bold tracking-wider">Tailwinds</h1>
+    </div>
+  );
+}
+
+export default App;
