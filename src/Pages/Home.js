58c490717f10e3a1b2c65c7b3383c21120e0294f
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-01-02 16:18:15 -0600

    Blocked out basic layout

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import Hero from "../Components/Home/hero.js";
+import ItemContainer from "../Components/Home/itemContainer.js";
+
 const Home = () => {
     return (
-        <div className="bg-green-300 w-full flex flex-row justify-center items-center py-8">
-            <h1 className="text-4xl font-bold text-black tracking-wider">Home Page</h1>
+        <div className="bg-green-300 w-full flex flex-col">
+            <Hero />
+            <ItemContainer />
         </div>
     )
 };
