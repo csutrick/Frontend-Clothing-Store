55e9111ceef669a15e9017b51343f44eca435dfe
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-01-03 23:53:45 -0600

    Added basic navbar search bar, updates state

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoIosSearch } from "react-icons/io";
 
-const Navbar = () => {
-    return (
-        <div className="bg-blue-300 w-full flex flex-row justify-center items-center py-4">
-            <h1 className="text-4xl font-bold text-black tracking-wider">Navbar</h1>
-        </div>
-    )
+const Navbar = ({ setSearchTerm }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSearch = () => {
+    setSearchTerm(inputValue); 
+  };
+
+  return (
+    <div className="w-full flex justify-center items-center py-2">
+      <div className="relative group drop-shadow-lg">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="rounded-full py-2 px-4 
+          border border-gray-300 bg-white focus:outline-none focus:border-blue-500 focus:ring focus:ring-blue-200"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          className="absolute right-0 top-0 h-full p-2
+          bg-blue-500 text-white rounded-full
+          hover:scale-105 active:scale-110" 
+          onClick={handleSearch}
+        >
+          <IoIosSearch className="text-2xl" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
