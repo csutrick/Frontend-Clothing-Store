381ff10590f76dce94b8200a71c1ae1b09549594
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-01-10 23:24:03 -0600

    New API server, built new fetchData

--- src/util/APIRequest.js
+++ src/util/APIRequest.js
@@ -1,15 +1,21 @@
-import axios from 'axios';
-
-async function APIRequest() {
-    const apiUrl = 'https://fakestoreapi.com/products';
+const fetchData = async (searchTerm) => {
+    const url = `https://kohls.p.rapidapi.com/categories/list?search=${encodeURIComponent(searchTerm)}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '',
+            'X-RapidAPI-Host': 'kohls.p.rapidapi.com'
+        }
+    };
 
     try {
-        const response = await axios.get(apiUrl);
-
-        return response.data;
+        const response = await fetch(url, options);
+        const result = await response.json();
+        return result;
     } catch (error) {
-        return { error: error.message };
+        console.error(error);
+        throw error;
     }
-}
+};
 
-export default APIRequest;
+export default fetchData;
