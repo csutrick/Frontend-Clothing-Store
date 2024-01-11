c7d7ef792204fe2a0e68d1a5941bfa3f3dedee8c
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2024-01-11 13:11:04 -0600

    Added searchTerm logic to apiFetch

--- src/Components/Navbar/categories/men.js
+++ src/Components/Navbar/categories/men.js
@@ -1,6 +1,17 @@
 import React from "react";
 
-const Mens = () => {
+import fetchData from "../../../util/fetchData.js";
+
+const Mens = ({ setSearchResults }) => {
+    const handleItemClick = async (searchTerm) => {
+        try {
+            const data = await fetchData(searchTerm);
+            setSearchResults(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     return (
         <div
         className="relative bg-transparent group">
@@ -9,9 +20,15 @@ const Mens = () => {
                 Men
             </button>
             <ul className="hidden group-hover:block">
-                <li className="block"><a href="#" className="px-4 py-2 whitespace-nowrap font-bold mb-2 text-lg">Option 1</a></li>
-                <li className="block"><a href="#" className="px-4 py-2 whitespace-nowrap font-bold mb-2 text-lg">Option 2</a></li>
-                <li className="block"><a href="#" className="px-4 py-2 whitespace-nowrap font-bold mb-0 text-lg">Option 3</a></li>
+                <li className="block">
+                    <a 
+                        href="#" 
+                        className="px-4 py-2 whitespace-nowrap font-bold mb-2 text-lg"
+                        onClick={() => handleItemClick('Shirts')}
+                    >
+                        Shirts
+                    </a>
+                </li>
             </ul>
         </div>
   );
